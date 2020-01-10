d8045249908346d13d1ad191cda7fc0a620e18f0
Kazim Walji <kwalji@bsd220.org>
2020-01-10 11:12:15 -0600

    Initial Commit

---
+++ DemoSimplexApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoSimplexApps
+//
+//  Created by Kazim Walji on 1/10/20.
+//  Copyright © 2020 HenryKazim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
